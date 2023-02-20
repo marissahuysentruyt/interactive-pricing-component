344d3855e7310bc8e59616e3de9e9d4b720b2cf3
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2023-02-20 16:46:58 -0500

    :wrench: chore: webpack config
    Add config for webpack support

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname + '/dist'),
+    filename: 'app_bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: 'index.html'})],
+}
