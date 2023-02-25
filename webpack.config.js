76ecfc4892a4f8aad5bfe8118f9d68102a489985
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2023-02-25 15:08:18 -0500

    :green_heart: fix: webpack config fixed
    Updated webpack.config to help load images better and added a --watch
    script to see updates faster.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
+  mode: "development",
   entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname + "/dist"),
-    filename: "app_bundle.js"
+    filename: "app_bundle.js",
+    clean: true,
   },
   module: {
     rules: [
@@ -21,7 +23,22 @@ module.exports = {
           "sass-loader",
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "index.html"})],
+  devServer: {
+    static: "./dist",
+    hot: true,
+  },
+  // optimization: {
+  //   runtimeChunk: "single",
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "index.html"}),
+    // Plugin for hot module replacement
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 };
