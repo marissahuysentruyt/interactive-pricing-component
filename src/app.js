5771c1df10e9029fa0a428fbe7d393dc00205b30
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2023-02-24 19:36:51 -0500

    :sparkles: feat: input values display
    The variables are saved and will get displayed on page.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-import "./scss/global.scss";
+// import './scss/global.scss';
+
+const pageviewCount = document.querySelector(".js-pageviews");
+pageviewCount.innerText = "100K ";
+
+const inputSlider = document.querySelector("input[type='range']");
+let price = inputSlider.value;
+const spanPriceDisplay = document.querySelector(".js-input-value");
+spanPriceDisplay.innerHTML = price;
+
+const getInputValue = (inputValue) => {
+  return inputValue;
+};
+
+inputSlider.addEventListener("input", function(e) {
+  return (
+    spanPriceDisplay.innerHTML = getInputValue(e.target.value)
+  );
+});
