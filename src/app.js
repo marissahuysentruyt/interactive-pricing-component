c6a6bf67af4d0f71cbd4faca9300958fc8d27ec3
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2023-02-24 20:43:35 -0500

    :sparkles: feat: pageview count updates
    New calculatePageviews function takes the slider input to calculate the
    number of pageviews per month.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 // import './scss/global.scss';
 
 const pageviewCount = document.querySelector(".js-pageviews");
-pageviewCount.innerText = "100K ";
+const pagesPerDollar = 6250;
+const calculatePageviews = (dollars) => {
+  return dollars * pagesPerDollar;
+};
+
+// 16 / 100K = price per page view (.00016)
+// 8 / 50000 === .00016
+// 100000 / 16 = 6250
+// .00016 * 6250 = 1
+// 6250 = 1
+// (6250 * dollars)
 
 const inputSlider = document.querySelector("input[type='range']");
 let price = inputSlider.value;
+pageviewCount.innerHTML = calculatePageviews(price);
 const spanPriceDisplay = document.querySelector(".js-input-value");
 spanPriceDisplay.innerHTML = price;
 
@@ -15,5 +26,11 @@ const getInputValue = (inputValue) => {
 inputSlider.addEventListener("input", function(e) {
   return (
     spanPriceDisplay.innerHTML = getInputValue(e.target.value)
+  );
+});
+
+inputSlider.addEventListener("change", function(e) {
+  return (
+    pageviewCount.innerHTML = calculatePageviews(e.target.value)
   );
 });
